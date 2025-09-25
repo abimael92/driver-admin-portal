411ee074191532e9ba5b3b4992beac1ec29f2b6a
Abimael Garcia <abimael1992g@gmail.com>
2025-09-24 19:05:56 -0600

    Update AppComponent to standalone with shared imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [FooterComponent, NavbarComponent, RouterOutlet],
 })
 export class AppComponent {
   title = 'ruta-facil';
