acf88bbcef4c85ff12328e614f355d63dbf2517a
Abimael Garcia <abimael1992g@gmail.com>
2025-09-24 18:24:43 -0600

    Add passenger booking feature

---
+++ src/app/features/passenger-booking/passenger-booking.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PassengerBookingRoutingModule } from './passenger-booking-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PassengerBookingRoutingModule
+  ]
+})
+export class PassengerBookingModule { }
