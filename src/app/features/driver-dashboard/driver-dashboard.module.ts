f15fc4dad7408baf635e5b9c5a595a6a6ac04d0d
Abimael Garcia <abimael1992g@gmail.com>
2025-09-24 18:24:42 -0600

    Add driver dashboard feature

---
+++ src/app/features/driver-dashboard/driver-dashboard.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DriverDashboardRoutingModule } from './driver-dashboard-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DriverDashboardRoutingModule
+  ]
+})
+export class DriverDashboardModule { }
