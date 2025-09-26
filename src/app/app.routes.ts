d4858d3544da569c2c68e1a899f0c93b01e129f4
Abimael Garcia <abimael1992g@gmail.com>
2025-09-26 15:41:05 -0600

    refactor: adjust app routes configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
+import { HelpComponent } from './components/help/help.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'help', component: HelpComponent },
+];
